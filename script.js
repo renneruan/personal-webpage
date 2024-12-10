a32c061113543e1d81984050fcf68e39c7b3b492
Rennê Ruan Alves Oliveira <renneruan@hotmail.com>
2024-12-09 21:10:02 -0300

    Alterações visuais e adições de seções de projetos e contato

--- script.js
+++ script.js
@@ -9,8 +9,18 @@
 //     }
 // });
 
-document.querySelector('.experience-btn').addEventListener('click', function () {
-    const dropdown = document.querySelector('.dropdown-text');
-    dropdown.classList.toggle('show'); // Add or remove the 'show' class
-  });
+// document.querySelector('.experience-btn').addEventListener('click', function () {
+//     const dropdown = document.querySelector('.dropdown-text');
+//     dropdown.classList.toggle('show'); // Add or remove the 'show' class
+//   });
   
+let prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0"; // Show navbar
+  } else {
+    document.querySelector("nav").style.top = "-70px"; // Hide navbar
+  }
+  prevScrollpos = currentScrollPos;
+};
